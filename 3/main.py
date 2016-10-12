5583fcdd9ee1f58c0624fee629fba1b6ea52f41f
bazingaterry <terry@32ph.com>
2016-10-12 14:10:36 +0800

    添加拉普拉斯平滑

--- 3/main.py
+++ 3/main.py
@@ -6,6 +6,8 @@ class Emotion(object):
         self.__emotion_id = emotion_id
         # 统计每个单词在该情感中出现次数
         self.__words_count = dict()
+        # 统计不重复单词
+        self.__words_set = set()
         # 单词总数，应该等于上面所有 key 对应 value 的 sum
         self.__total_words = 0
         # 这个情感在测试集出现的次数
@@ -17,6 +19,7 @@ class Emotion(object):
         else:
             self.__words_count[word] = n
         self.__total_words += n
+        self.__words_set.add(word)
 
     @property
     def id(self):
@@ -41,14 +44,22 @@ class Emotion(object):
         Emotion.__global_count += 1
         self.__self_count += 1
 
-    def get_word_probability(self, word):
+    def get_word_probability(self, word, laplace_smoothing=False):
         """
+        :param word: 一个测试集单词
+        :param laplace_smoothing: 是否开启拉普拉斯平滑
         :return: 返回该单词在该情感中出现的未归一化概率
         """
-        if word in self.__words_count:
-            return self.__words_count[word] / self.__total_words
+        if laplace_smoothing is True:
+            if word in self.__words_count:
+                return self.__words_count[word] + 1 / self.__total_words + len(self.__words_set)
+            else:
+                return 1 / self.__total_words + len(self.__words_set)
         else:
-            return 0
+            if word in self.__words_count:
+                return self.__words_count[word] / self.__total_words
+            else:
+                return 0
 
 
 def read_file(file_name):
@@ -58,7 +69,7 @@ def read_file(file_name):
             yield int(line[1]), line[3:]
 
 
-def main():
+def classification():
     emotions = dict()
     for i in range(1, 7):
         emotions[i] = Emotion(i)
@@ -66,21 +77,26 @@ def main():
         emotions[emotion_id].appear()
         for word in words_list:
             emotions[emotion_id].add(word)
+    rig = 0
+    wro = 0
     for answer_id, words_list in read_file('Classification/test.txt'):
         max_probability_id = None
         max_probability = 0
         for emotion_id, emotion in emotions.items():
             p = 1
             for word in words_list:
-                p *= emotion.get_word_probability(word)
+                p *= emotion.get_word_probability(word, laplace_smoothing=True)
             if p > max_probability:
                 max_probability = p
                 max_probability_id = emotion_id
         if max_probability_id == answer_id:
             print('Hhh', max_probability)
+            rig += 1
         else:
             print('ToT', max_probability)
+            wro += 1
+    print(rig, wro, rig + wro)
 
 
 if __name__ == '__main__':
-    main()
+    classification()
