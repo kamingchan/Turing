4c0710247137e51553e34265c5678591f0f4d9c5
bazingaterry <terry@32ph.com>
2016-10-12 13:05:46 +0800

    sync

--- 3/main.py
+++ 3/main.py
@@ -22,6 +22,10 @@ class Emotion(object):
     def id(self):
         return self.__emotion_id
 
+    # todo： 这个接口不一定有用
+    # @property
+    # def size(self):
+    #     return self.__total_words
 
     @property
     def probability(self):
@@ -47,8 +51,36 @@ class Emotion(object):
             return 0
 
 
-def read_file():
-    with open('Classification/train.txt') as f:
+def read_file(file_name):
+    with open(file_name) as f:
         for line in f.readlines()[1:]:
             line = line.replace('\n', '').split(' ')
             yield int(line[1]), line[3:]
+
+
+def main():
+    emotions = dict()
+    for i in range(1, 7):
+        emotions[i] = Emotion(i)
+    for emotion_id, words_list in read_file('Classification/train.txt'):
+        emotions[emotion_id].appear()
+        for word in words_list:
+            emotions[emotion_id].add(word)
+    for answer_id, words_list in read_file('Classification/test.txt'):
+        max_probability_id = None
+        max_probability = 0
+        for emotion_id, emotion in emotions.items():
+            p = 1
+            for word in words_list:
+                p *= emotion.get_word_probability(word)
+            if p > max_probability:
+                max_probability = p
+                max_probability_id = emotion_id
+        if max_probability_id == answer_id:
+            print('Hhh', max_probability)
+        else:
+            print('ToT', max_probability)
+
+
+if __name__ == '__main__':
+    main()
